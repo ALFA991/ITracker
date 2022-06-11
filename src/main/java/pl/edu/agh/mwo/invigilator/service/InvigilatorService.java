735b0f3bfb16662f6cd5f81239521e4a8a99c130
ArturSobieraj <artur.sobieraj126@gmail.com>
2022-06-11 14:08:46 +0200

    Added method mocks

--- src/main/java/pl/edu/agh/mwo/invigilator/service/InvigilatorService.java
+++ src/main/java/pl/edu/agh/mwo/invigilator/service/InvigilatorService.java
@@ -9,16 +9,16 @@ public class InvigilatorService {
     public void startInvigilation(Commands commandToResolve) {
         switch (commandToResolve) {
             case INV1: {
-
+                System.out.println("Wybrano raport 1. Jest w trakcie implementacji");
             }
             case INV2: {
-
+                System.out.println("Wybrano raport 2. Jest w trakcie implementacji");
             }
             case INV3: {
-
+                System.out.println("Wybrano raport 3. Jest w trakcie implementacji");
             }
             case HELP: {
-
+                System.out.println("Wybrano pomoc. Jest w trakcie implementacji");
             }
         }
     }

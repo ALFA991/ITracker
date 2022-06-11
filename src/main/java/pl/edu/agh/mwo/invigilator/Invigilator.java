b456519ba8f9f759afd4a90684220ca79006a967
ArturSobieraj <artur.sobieraj126@gmail.com>
2022-06-11 13:56:16 +0200

    #FP19 Create command for raport 1

--- src/main/java/pl/edu/agh/mwo/invigilator/Invigilator.java
+++ src/main/java/pl/edu/agh/mwo/invigilator/Invigilator.java
@@ -1,6 +1,16 @@
 package pl.edu.agh.mwo.invigilator;
 
+import pl.edu.agh.mwo.invigilator.service.InvigilatorService;
+import pl.edu.agh.mwo.invigilator.service.Utils;
+
 public class Invigilator {
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        InvigilatorService invigilatorService = new InvigilatorService();
+        if (args.length > 1) {
+            throw new IllegalStateException("Bad command");
+        } else {
+            invigilatorService.startInvigilation(Utils.parseCommandToConstant(args[0]));
+        }
+    }
 }
---
+++ src/main/java/pl/edu/agh/mwo/invigilator/service/InvigilatorService.java
@@ -0,0 +1,25 @@
+package pl.edu.agh.mwo.invigilator.service;
+
+import lombok.NoArgsConstructor;
+import pl.edu.agh.mwo.invigilator.model.Commands;
+
+@NoArgsConstructor
+public class InvigilatorService {
+
+    public void startInvigilation(Commands commandToResolve) {
+        switch (commandToResolve) {
+            case INV1: {
+
+            }
+            case INV2: {
+
+            }
+            case INV3: {
+
+            }
+            case HELP: {
+
+            }
+        }
+    }
+}

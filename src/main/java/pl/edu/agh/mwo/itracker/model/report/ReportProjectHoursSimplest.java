d6faa2473cd259bbd89339ef484da5c7b65e6b1f
michalrys <michalrys@gmail.com>
2022-06-12 10:50:04 +0200

    FP-9: report 2 - ReportProjectHoursSimplest -> add collection of project / hours

--- src/main/java/pl/edu/agh/mwo/itracker/model/report/ReportProjectHoursSimplest.java
+++ src/main/java/pl/edu/agh/mwo/itracker/model/report/ReportProjectHoursSimplest.java
@@ -12,6 +12,7 @@ import java.util.Map;
 public class ReportProjectHoursSimplest implements Report {
     private String name;
     private Map<String, Employee> employees = new HashMap<>();
+    private Map<Project, Double> projectTotalHours = new HashMap<>();
 
     public ReportProjectHoursSimplest(String name) {
         this.name = name;
@@ -41,37 +42,30 @@ public class ReportProjectHoursSimplest implements Report {
         double currentTotalHours = project.getTotalHours();
         project.setTotalHours(currentTotalHours + totalHours);
         employee.setProject(project);
+
+        projectTotalHours.put(project, currentTotalHours + totalHours);
     }
 
     @Override
     public String getSummaryForConsole() {
         StringBuilder summary = new StringBuilder();
-        summary.append("Report type 2:\n");
+        summary.append("Report type 2:  (project/hours)\n");
         summary.append("Created in: " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("YYYY-MM-dd HH:mm:ss")));
         summary.append(" by " + System.getProperty("user.name") + "\n");
         summary.append("Title: " + name + "\n");
         summary.append("--------------------------------------\n");
 
-
-
-
-//        for (Map.Entry<String, Employee> entry : employees.entrySet()) {
-//            summary.append(entry.getKey() + " have ");
-//            Employee employee = entry.getValue();
-//            Map<String, Project> projects = employee.getProjects();
-//            double totalHoursForAllProjects = 0;
-//            for (String projectName : projects.keySet()) {
-//                Project project = projects.get(projectName);
-//                totalHoursForAllProjects += project.getTotalHours();
-//            }
-//            summary.append(totalHoursForAllProjects);
-//            summary.append(" hours.\n");
-//        }
+        for (Project project : projectTotalHours.keySet()) {
+            summary.append(project.getName());
+            summary.append(" have ");
+            summary.append(project.getTotalHours());
+            summary.append(" hours.\n");
+        }
         return summary.toString();
     }
 
     @Override
     public String toString() {
-        return "<Report 1 simplest: " + name + ">";
+        return "<Report 2 simplest: " + name + ">";
     }
 }

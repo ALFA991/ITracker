ab1a5a44b9aba974395270e6291f32b71537a153
stefania <stefania@d17-428-pc-17.ipa.ki.agh.edu.pl>
2022-06-12 09:53:06 +0200

    Report3Final

--- src/main/java/pl/edu/agh/mwo/invigilator/Report3.java
+++ src/main/java/pl/edu/agh/mwo/invigilator/Report3.java
@@ -9,11 +9,22 @@ import java.io.IOException;
 import java.util.*;
 
 public class Report3 {
-    public static Map<String,Double> employee = new HashMap<>();
+    static Map<String,EmployeeReport3> employeeMap = new HashMap<String,EmployeeReport3>();
+    static List<EmployeeReport3> EmployeeReport3ObjList = new ArrayList<EmployeeReport3>();
+    static EmployeeReport3 record;
 
+    public static void main(String[] args) throws FileNotFoundException {
+        executeReport3("/home/students/s/t/stefania/Documents/Projekt/invigilator/src/main");
+        System.out.println("Report 3 results: \n");
+        for (int i = 0;i < EmployeeReport3ObjList.size();i++){
+            record = EmployeeReport3ObjList.get(i);
+            System.out.println(record.toString());
+        }
+    }
     public static void executeReport3(String path) throws FileNotFoundException {
         List<String> fileList = new ArrayList<>();
         getAllFilesRep3(path, fileList);
+
     }
     private static List<String> getAllFilesRep3(String path, List<String> fileList) throws FileNotFoundException {
         File[] allFiles = new File(path).listFiles();
@@ -35,25 +46,51 @@ public class Report3 {
             HSSFSheet sheet;
             POIFSFileSystem fs = new POIFSFileSystem(new FileInputStream(filepath));
             HSSFWorkbook wb = new HSSFWorkbook(fs);
+
             int noOfSheets = wb.getNumberOfSheets();
             File file = new File(filepath);
             String fileName = file.getName();
             String fileNameWithoutExtension = fileName.substring(0, fileName.lastIndexOf('.'));
-            employee.put("typ",0.0);
-            System.out.println("\nProjects employee: " + fileNameWithoutExtension);
+
             for(int i = 0; i < noOfSheets; i++){
+                double sumOfHours = 0;
                 sheet = wb.getSheetAt(i);
                 String sheetName = sheet.getSheetName();
-                double sumOfHours = 0;
-                System.out.print(sheetName + ": ");
+                String keyTxt = fileNameWithoutExtension + sheetName;
+
+                record = employeeMap.get(keyTxt);
+                if (record == null){
+                    record = new EmployeeReport3();
+                    record.setEmployeeName(fileNameWithoutExtension);
+                    EmployeeReport3ObjList.add(record);
+
+                }
+                String projName = record.getProjectName();
+                if (projName == null){
+                    record.setProjectName(sheetName);
+                    employeeMap.put(keyTxt,record);
+                    projName = sheetName;
+                }
+                if (!projName.contains(sheetName)){
+                    record = new EmployeeReport3();
+                    EmployeeReport3ObjList.add(record);
+                    record.setEmployeeName(fileNameWithoutExtension);
+                    record.setProjectName(sheetName);
+                    employeeMap.put(keyTxt,record);
+
+                }
+
+
+
+
+
                 HSSFRow row = sheet.getRow(0);
                 int rowNo = 1;
                 while (sheet.getRow(rowNo) != null) {
                     double hours = sheet.getRow(rowNo).getCell(2).getNumericCellValue();
-                    employee.merge(fileNameWithoutExtension, hours,Double::sum);
                     sumOfHours += sheet.getRow(rowNo).getCell(2).getNumericCellValue();
+                    record.setWorkedHours(sumOfHours);
                     rowNo++;
-                    System.out.println(employee.get(fileNameWithoutExtension));
                 }
                 //System.out.println(sumOfHours + " hours.");
 
